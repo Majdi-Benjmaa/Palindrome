91a732785219f6e354c74af12cbdcf43f03a9614
Majdi Benjmaa <Majdi.Benjmaa@cho.com.tn>
2023-11-10 09:12:50 +0100

    V2.0

--- palindrome.js
+++ palindrome.js
@@ -1,46 +1,63 @@
 function findPalindromes(paragraph) {
-  // Define a helper function to check if a word contains a palindrome
-  function hasPalindrome(word) {
+  function isPalindrome(word) {
     const cleanedWord = word.replace(/[^a-zA-Z]/g, "").toLowerCase();
     return cleanedWord === cleanedWord.split('').reverse().join('');
   }
 
-  // Split the paragraph into words using spaces, commas, and periods as separators
-  let words = paragraph.split(/\s|,\s*|\.\s*/);
+  function findPalindromicWords(words) {
+    return words.filter(word => word.length > 1 && isPalindrome(word));
+  }
 
-  // Remove empty strings from the array of words
-  words = words.filter(word => word);
+  function findPalindromicPhrases(words) {
+    let phrases = [];
+    for (let i = 0; i < words.length; i++) {
+      for (let j = i + 1; j <= words.length; j++) {
+        const phrase = words.slice(i, j).join(' ');
+        if (isPalindrome(phrase)) {
+          phrases.push(phrase);
+        }
+      }
+    }
+    return phrases;
+  }
 
-  // Find words containing palindromes in the array of words
-  const palindromes = words.filter(word => word.length > 1 && hasPalindrome(word));
+  const cleanedParagraph = paragraph.replace(/[.,]/g, ''); // Remove periods and commas for paragraph palindrome check
+  const words = cleanedParagraph.split(/\s+/).filter(word => word);
 
-  return palindromes;
+  const palindromicWords = findPalindromicWords(words);
+  const palindromicPhrases = findPalindromicPhrases(words);
+
+  return {
+    palindromicParagraph: isPalindrome(cleanedParagraph) ? cleanedParagraph : null,
+    palindromicPhrases: palindromicPhrases.length > 0 ? palindromicPhrases : null,
+    palindromicWords: palindromicWords.length > 0 ? palindromicWords : null
+  };
 }
-// // Example usage:
-  // const paragraph = "A man, a plan, a canal, Panama. Racecar is a palindrome.";
-  // const result = findPalindromes(paragraph);
-  
-  // // Display the result in the HTML document
-  // const outputElement = document.getElementById("output");
-  // outputElement.textContent = "Palindromes: " + result.join(', ');
 
 function findAndDisplayPalindromes() {
-  // Get the input text from the user
   const userInput = document.getElementById("textInput").value.trim();
 
-  // Check if the text input is empty
   if (!userInput) {
-    // Display a message if the input is empty
     const outputElement = document.getElementById("output");
-    outputElement.textContent = "Please write something!";
+    outputElement.innerHTML = "<span class='output-label'>Please write something!</span>";
   } else {
-    // Find palindromes using the existing function
     const result = findPalindromes(userInput);
 
-    // Display the result in the HTML document
     const outputElement = document.getElementById("output");
-    outputElement.textContent = result.length > 0
-      ? "Palindromes: " + result.join(', ')
-      : "No palindromes found.";
+    let outputText = "";
+
+    if (result.palindromicParagraph) {
+      outputText += "<span class='output-label'>Palindrome Paragraph:</span> " + result.palindromicParagraph + "<br><br>";
+    }
+
+    if (result.palindromicPhrases) {
+      outputText += "<span class='output-label'>Palindrome Phrases:</span><br>" + result.palindromicPhrases.map(phrase => "  " + phrase).join('<br>') + "<br><br>";
+    }
+
+    if (result.palindromicWords) {
+      outputText += "<span class='output-label'>Palindrome Words:</span> " + result.palindromicWords.join(', ');
+    }
+
+    outputElement.innerHTML = outputText || "<span class='output-label'>No palindromes found.</span>";
   }
 }
